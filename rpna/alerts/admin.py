72e35644ec0660be8bbb171ac9c03ae4def8ab4c
Jace Browning <jacebrowning@gmail.com>
2021-11-06 10:04:48 -0400

    Add Event model

---
+++ rpna/alerts/admin.py
@@ -0,0 +1,20 @@
+# pylint: disable=unused-argument
+
+from django.contrib import admin
+
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+
+    list_filter = ["sent"]
+    list_display = [
+        "id",
+        "sent",
+    ]
+
+    # TODO: Add admin action to send alerts
+    # actions = [sent_alert]
+
+    readonly_fields = ["sent"]
--- rpna/alerts/models.py
+++ rpna/alerts/models.py
@@ -1,3 +1,13 @@
 # pylint: disable=unused-import
 
 from django.contrib.auth.models import User
+from django.db import models
+
+
+class Event(models.Model):
+
+    sent = models.BooleanField(
+        default=False,
+        editable=False,
+        help_text="Indicates alerts has been scheduled to be sent.",
+    )
