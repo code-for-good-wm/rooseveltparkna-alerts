b324ecfb10dfc5fbb092d09482ec8ccab05a8ff5
Jace Browning <jacebrowning@gmail.com>
2021-11-06 13:46:02 -0400

    Load domain from the environment

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ import dj_database_url
 from .default import *
 
 BASE_NAME = os.environ["HEROKU_APP_NAME"]
-BASE_DOMAIN = f"{BASE_NAME}.com"
+BASE_DOMAIN = BASE_NAME
 BASE_URL = f"https://{BASE_DOMAIN}"
 
 ###############################################################################
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 ALLOWED_HOSTS = [
     "0.0.0.0",
     "localhost",
-    ".herokuapp.com",  # TODO: Remove this line and add your custom domain
+    ".rooseveltparkna.org",
 ]
 
 ###############################################################################
