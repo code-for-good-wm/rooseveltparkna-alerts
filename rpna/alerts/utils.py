bb22150141dd9448004a04df66d9ce0f353a3c10
Jace Browning <jacebrowning@gmail.com>
2021-11-08 11:30:37 -0500

    Only create a Twilio client when credentials are set

--- rpna/alerts/utils.py
+++ rpna/alerts/utils.py
@@ -26,14 +26,17 @@ def generate_code(user):
 
 def send_text_message(number: str, message: str) -> bool:
     success = False
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    try:
-        message = client.messages.create(
-            to=number, from_=settings.TWILIO_NUMBER, body=message
-        )
-    except TwilioRestException as e:
-        log.error(e)
+    if settings.TWILIO_ACCOUNT_SID:
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        try:
+            message = client.messages.create(
+                to=number, from_=settings.TWILIO_NUMBER, body=message
+            )
+        except TwilioRestException as e:
+            log.error(e)
+        else:
+            log.info(f"Sent text message (sid={message.sid})")  # type: ignore
+            success = True
     else:
-        log.info(f"Sent text message (sid={message.sid})")  # type: ignore
-        success = True
+        log.error("Twilio credentials are not set")
     return success
